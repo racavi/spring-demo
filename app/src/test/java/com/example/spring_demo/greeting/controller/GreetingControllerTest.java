0a9f0dfcede21e6c58d94551064583ec2bb56934
Rafa Calvo <63754467+racavi@users.noreply.github.com>
2023-03-09 18:17:01 +0100

    Add default package visibility to JUnit5 test class

--- app/src/test/java/com/example/spring_demo/greeting/controller/GreetingControllerTest.java
+++ app/src/test/java/com/example/spring_demo/greeting/controller/GreetingControllerTest.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.BDDAssertions.then;
 @ExtendWith(SpringExtension.class)
 @AutoConfigureJsonTesters
 @WebMvcTest(GreetingController.class)
-public class GreetingControllerTest {
+class GreetingControllerTest {
     @Autowired
     private MockMvc mvc;
 
